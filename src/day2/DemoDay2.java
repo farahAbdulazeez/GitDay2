d2f4d55359d49f6418b7bd5daaccaea28cc5b367
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-01-31 17:19:02 -0600

    practice

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,7 +4,10 @@ public class DemoDay2 {
 	public static void main(String args []) {
 		
 		System.out.println("Farah");
-		
+		 int a =10;
+		 int b = 20;
+		 
+		 System.out.println(a +b);
 	}
 
 }
